ca3860224008aa41b28bfe4e233308b950c8bac8
nevayildiz <250541034@firat.edu.tr>
2025-10-21 23:18:25 +0300

    Update AsciiArtHarf.java

---
+++ Task-4-StringDesenler/AsciiArtHarf.java
@@ -0,0 +1,15 @@
+public class ASCIIArt {
+    public static void main(String[] args) {
+        System.out.println(" N harfi (ASCII sanatı):\n");
+        System.out.println("@          @    ");
+        System.out.println("@ @        @    ");
+        System.out.println("@  @       @    ");
+        System.out.println("@   @      @    ");
+        System.out.println("@    @     @    ");
+        System.out.println("@     @    @    ");
+        System.out.println("@      @   @    ");
+        System.out.println("@       @  @    ");
+        System.out.println("@        @ @    ");
+
+    }
+}
