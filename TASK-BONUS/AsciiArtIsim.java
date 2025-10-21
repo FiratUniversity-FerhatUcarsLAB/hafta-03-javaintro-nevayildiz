5fd4dd420387fe25f802dd67d376879ead87bb74
nevayildiz <250541034@firat.edu.tr>
2025-10-21 23:42:32 +0300

    Update AsciiArtIsim.java

---
+++ TASK-BONUS/AsciiArtIsim.java
@@ -0,0 +1,9 @@
+public class ASCIISanat {
+    public static void main(String[] args) {
+        System.out.println("@   @  @@@@@  @   @  @@@@@");
+        System.out.println("@@  @  @      @   @  @   @");
+        System.out.println("@ @ @  @@@@   @   @  @@@@@");
+        System.out.println("@  @@  @      @ @ @  @   @");
+        System.out.println("@   @  @@@@@   @ @   @   @");
+    }
+}
