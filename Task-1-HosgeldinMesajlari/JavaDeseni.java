f335d6bb8016a90a4fcfca11199c3867fbda8f0a
nevayildiz <250541034@firat.edu.tr>
2025-10-21 22:26:22 +0300

    Update JavaDeseni.java

---
+++ Task-1-HosgeldinMesajlari/JavaDeseni.java
@@ -0,0 +1,8 @@
+public class DesenCizme {
+    public static void main(String[] args) {
+        System.out.println("        J        ");
+        System.out.println("      A   A   ");
+        System.out.println("    V       V");
+        System.out.println("  A           A");
+    }
+}
