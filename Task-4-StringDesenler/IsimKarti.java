12a720a10496a8ad2784b6a00bbaaadf24d43bc1
nevayildiz <250541034@firat.edu.tr>
2025-10-21 23:23:58 +0300

    Update IsimKarti.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class IsimKarti {
+    public static void main(String[] args) {
+        String isim = " Neva ";
+        String soyisim = " Yıldız ";
+        String öğrenciNo = " 250541034 ";
+        String bolum = " Yazılım Mühendisliği ";
+        System.out.println("İsim      : " + isim);
+        System.out.println("Soyisim   : " + soyisim);
+        System.out.println("Öğrenci No: " + öğrenciNo);
+        System.out.println("Bolum     : " + bolum);
+    }
+
+}
